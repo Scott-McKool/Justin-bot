363aefb491577edb43ab18c1a3c63b558aad4dd7
Scott-McKool <scott.lr.cool@gmail.com>
2022-03-06 20:53:23 -0500

    rebalanced robbing

--- cogs/coins.py
+++ cogs/coins.py
@@ -152,8 +152,8 @@ class Coins(commands.Cog):
             return await ctx.send("you failed to rob "+str(member))
         authorCoins = self.getCoins(author.id)
         userCoins = self.getCoins(member.id)
-        # get te diff between the attacker and victim. reduce the victim's wealth to nerf rich ppl. then divide by 500
-        coinDiff = ((userCoins*0.95)-authorCoins)/self.startingCoins
+        # get te diff between the attacker and victim. then divide by 500
+        coinDiff = ((userCoins)-authorCoins)/self.startingCoins
         # multiply by 5 to get a sizable ammount of coins
         coinDiff = (coinDiff * 5)
         # make the diff the center of a normal distribution to add some randomness to the system
