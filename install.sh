1d2233e01f455f5897361f74f1ef13ce97a85fd9
Scott-McKool <scott.lr.cool@gmail.com>
2023-07-02 15:05:33 -0400

    made justinbot not rely on root

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 #    sets up systemd to run justinbot on startup
 
 # check for root (user id of 0)
- [ `id -u` -ne 0 ] && echo "this script should be run as root" && exit 1
+ [ `id -u` -eq 0 ] && echo "this script should not be run as root" && exit 1
 
 ### make and set up config file
 # get discord token from user
@@ -23,11 +23,12 @@ CONFIGFILE
 
 ### install requirements
 echo "installing pip with apt. . ."
-apt update && apt install python3-pip -y
+sudo apt update && sudo apt install python3-pip -y || { echo "failed to install python3-pip, aborting. . ."; exit 1; }
 
 # install python packages
 echo "installing requirements with pip3. . ."
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt || { echo "failed to install pip packages, aborting. . ."; exit 1; }
+
 
 ### instal files
 # get files from release page of github when i put them there in the future LOLOLOLOLOL
@@ -38,21 +39,25 @@ echo "Setting up justin.py to run on system startup"
 echo "creating unit file 'justinbot.service'"
     cat > justinbot.service <<UNITFILE
 [Unit]
-Description=Runs justin.py script on startup
-After=multi-user.target
+Description=Runs justin bot on startup
+Wants=network-online.target
+After=network-online.target
 [Service]
+User=$(whoami)
+Group=$(id -gn)
 ExecStart=/usr/bin/python3 "$PWD/justin.py"
 [Install]
 WantedBy=multi-user.target
 UNITFILE
 # put the unitfile in its place w/ systemd
-mv justinbot.service /etc/systemd/system/justinbot.service
+sudo mv justinbot.service /etc/systemd/system/justinbot.service
 # reload systemd so it can find this newly created service
 echo "reloding systemctl daemon"
-systemctl daemon-reload
+sudo systemctl daemon-reload
 # enable this service in systemd
 echo "enabling justinbot.service"
 sudo systemctl enable justinbot.service
+
 echo "justin bot service has been added and enabled"
 echo "justin.py will be automatically run on startup from now on"
 echo ""
